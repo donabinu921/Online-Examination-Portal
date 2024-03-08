4dc5a09e71cd982b946d3cfeb4a027f2baa9b361
donabinu921 <elizabethdona921@gmail.com>
2024-03-08 12:41:44 +0530

    Login/Sign Up page set up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import LoginSignup from './Components/LoginSignup/LoginSignup';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <LoginSignup />
+    </div>
+  );
+}
+
+export default App;
